76cfbb4a487cc75d857ba7ff5838226f4eda31ba
Vladimir Kulgun <koulgoun@yandex.ru>
2024-02-21 17:41:09 +0400

    initial

---
+++ index.js
@@ -0,0 +1,16 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.TELEGRAM_BOT_TOKEN;
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(chatId, 'Received your message');
+});
