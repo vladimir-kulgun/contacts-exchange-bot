edba7f54f834508f839bb25eae9ffa8d83ea58fd
Vladimir Kulgun <koulgoun@yandex.ru>
2024-02-22 19:28:36 +0400

    bot

--- index.js
+++ index.js
@@ -10,20 +10,80 @@ const webAppUrl = process.env.WEB_APP_URL;
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', async (msg) => {
-    const chatId = msg.chat.id;
-    const text = msg.text;
+const mainMenu = 'Главная';
+const createMeeting = 'Создать встречу';
+const listMeetings = 'Мои встречи';
+const about = 'О программе';
 
-    if (text === '/start') {
-        await bot.sendMessage(chatId, 'press menu', {
+bot.on('text', async msg => {
+    try {
+        const chatId = msg.chat.id;
+        const text = msg.text;
+
+        if(text.startsWith('/start')) {
+            const options = [createMeeting, listMeetings, about].map(o =>
+                [{ text: o, callback_data: o }]
+            );
+            await bot.sendMessage(chatId, mainMenu, {
+                reply_markup: {
+                    inline_keyboard: options,
+                    resize_keyboard: true
+                }
+            })
+        }
+    }
+    catch(error) {
+        console.log(error);
+    }
+})
+
+bot.on('callback_query', async (callbackQuery) => {
+    const action = callbackQuery.data;
+    const msg = callbackQuery.message;
+    const chatId = msg.chat.id;
+
+    if (action === mainMenu) {
+        const options = [createMeeting, listMeetings, about].map(o =>
+            [{ text: o, callback_data: o }]
+        );
+        await bot.sendMessage(chatId, mainMenu, {
             reply_markup: {
-                inline_keyboard: [
-                    [{text:'Menu', web_app: {url: webAppUrl}}]
-                ]
+                inline_keyboard: options,
+                resize_keyboard: true
             }
         })
     }
-
+    else if (action === createMeeting) {
+        const options = [mainMenu].map(o =>
+            [{ text: o, callback_data: o }]
+        );
+        await bot.sendMessage(chatId, createMeeting, {
+            reply_markup: {
+                inline_keyboard: options,
+                resize_keyboard: true,
+            }
+        })
+    }
+    else if (action === listMeetings) {
+        const options = [mainMenu].map(o =>
+            [{ text: o, callback_data: o }]
+        );
+        await bot.sendMessage(chatId, listMeetings, {
+            reply_markup: {
+                inline_keyboard: options,
+                resize_keyboard: true,
+            }
+        })
+    }
+    else if (action === about) {
+        const options = [mainMenu].map(o =>
+            [{ text: o, callback_data: o }]
+        );
+        await bot.sendMessage(chatId, about, {
+            reply_markup: {
+                inline_keyboard: options,
+                resize_keyboard: true,
+            }
+        })
+    }
 });
