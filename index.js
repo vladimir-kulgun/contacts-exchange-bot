d9abd994380c985e646bf149276b82083c8cd509
Vladimir Kulgun <koulgoun@yandex.ru>
2024-02-22 16:22:44 +0400

    menu

--- index.js
+++ index.js
@@ -5,15 +5,25 @@ dotenv.config();
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TELEGRAM_BOT_TOKEN;
+const webAppUrl = process.env.WEB_APP_URL;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
 // Listen for any kind of message. There are different kinds of
 // messages.
-bot.on('message', (msg) => {
+bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
+    const text = msg.text;
 
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message');
+    if (text === '/start') {
+        await bot.sendMessage(chatId, 'press menu', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text:'Menu', web_app: {url: webAppUrl}}]
+                ]
+            }
+        })
+    }
+
 });
